61b4cb428fd32294f6bcec5ebfdbd9ddc08cca0f
macjonessoriano <sorianomacjones@gmail.com>
2023-03-30 10:22:19 +0800

    final commit

--- dist/index.c36f364e.js
+++ dist/index.c36f364e.js
@@ -1,6 +1,24 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
+const boardWidth = 560;
+const boardHeight = 300;
+let timerId;
+let xDirection = -2;
+let yDirection = 2;
+let score = 0;
+const userStart = [
+    230,
+    10
+];
+let currentPosition = userStart;
+const ballStart = [
+    270,
+    40
+];
+let ballCurrentPosition = ballStart;
 // create Block
 class Block {
     constructor(xAxis, yAxis){
@@ -52,11 +70,84 @@ const drawBlocks = function() {
 };
 drawBlocks();
 // add user
-const user = document.createElement("div"); // const base_url = window.location.origin;
- // const host = window.location.host;
- // const pathArray = window.location.pathname.split('/');
- // console.log(base_url);
- // console.log(host);
- // console.log(pathArray);
+const user = document.createElement("div");
+user.classList.add("user");
+drawUser();
+grid.appendChild(user);
+// draw user
+function drawUser() {
+    user.style.left = currentPosition[0] + "px";
+    user.style.bottom = currentPosition[1] + "px";
+}
+// draw ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + "px";
+    ball.style.bottom = ballCurrentPosition[1] + "px";
+}
+// move user
+const moveUser = function(e) {
+    switch(e.key){
+        case "ArrowLeft":
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser();
+            }
+            break;
+        case "ArrowRight":
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10;
+                drawUser();
+            }
+            break;
+    }
+};
+document.addEventListener("keydown", moveUser);
+// add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+drawBall();
+grid.appendChild(ball);
+// move ball
+const moveBall = function() {
+    ballCurrentPosition[0] += xDirection;
+    ballCurrentPosition[1] += yDirection;
+    drawBall();
+    checkForCollisions();
+};
+timerId = setInterval(moveBall, 30);
+// check for collisions
+function checkForCollisions() {
+    // check for block collisions
+    for(let i = 0; i < blocks.length; i++)if (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0] && ballCurrentPosition[1] + ballDiameter > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) {
+        const allBlock = Array.from(document.querySelectorAll(".block"));
+        allBlock[i].classList.remove("block");
+        blocks.splice(i, 1);
+        changeDirection();
+        score++;
+        scoreDisplay.innerHTML = score;
+        // check for win
+        if (blocks.length === 0) {
+            scoreDisplay.innerHTML = "YOU WIN";
+            clearInterval(timerId);
+            document.removeEventListener("keydown", moveUser);
+        }
+    }
+    // check for wall collisions
+    if (ballCurrentPosition[0] >= boardWidth - ballDiameter || ballCurrentPosition[1] >= boardHeight - ballDiameter || ballCurrentPosition[0] <= 0) changeDirection();
+    // check for user collisions
+    if (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth && ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight) changeDirection();
+    // check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId);
+        scoreDisplay.innerHTML = "You lose";
+        document.removeEventListener("keydown", moveUser);
+    }
+}
+function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) return yDirection = -2;
+    if (xDirection === 2 && yDirection === -2) return xDirection = -2;
+    if (xDirection === -2 && yDirection === -2) return yDirection = 2;
+    if (xDirection === -2 && yDirection === 2) return xDirection = 2;
+}
 
 //# sourceMappingURL=index.c36f364e.js.map
